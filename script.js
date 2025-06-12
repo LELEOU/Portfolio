2b63189ef2c8892a0570b9431591cfeb71007b79
LELEOU <leonardogoncalves544@gmail.com>
2025-06-12 18:40:38 -0300

    Meu portifolio (falta os links dos projetos)

---
+++ script.js
@@ -0,0 +1,12 @@
+// Apenas efeitos de scroll
+ScrollReveal({
+  distance: '40px',
+  duration: 1000,
+  easing: 'ease-out',
+  origin: 'bottom',
+  reset: false
+});
+
+ScrollReveal().reveal('section', {
+  interval: 150
+});
